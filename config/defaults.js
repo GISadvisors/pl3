49088a6e652116bad3c8b523d4fb7d8810c2fdba
abuthaya <91283874+abuthaya@users.noreply.github.com>
2022-03-03 12:41:16 -0500

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -102,7 +102,7 @@ define({
     "searchAlwaysExpanded": false, // Whether or not search button is always expanded (always shows its type-in box; true) or is dynamically expanded (false)
     "bingKey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
     //Defaults to arcgis.com. Set this value to your portal or organization host name.
-    "sharinghost": location.protocol + "//" + "www.arcgis.com",
+    "sharinghost": location.protocol + "//" + "www.fleming.maps.arcgis.com",
     "units": null,
     //If your application needs to edit feature layer fields set this value to true. When false the map will
     //be created with layers that are not set to editable which allows the FeatureLayer to load features optimally.
